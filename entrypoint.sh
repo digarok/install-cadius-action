64996b30e846b9fc34c87415c9596746e343c0e7
Dagen Brock <dagenbrock@gmail.com>
2020-03-04 09:40:53 -0600

    2

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 CADZIP=https://github.com/digarok/cadius/releases/download/0.0.0/cadius-ubuntu-latest-0.0.0.zip
 
 curl -L $CADZIP -o cadius.zip
-unzip cadius.zip
+unzip -n cadius.zip
 mv cadius /usr/bin
 
 cadius createvolume $INPUT_VOLUME_PATH $INPUT_VOLUME_NAME $INPUT_VOLUME_SIZE
@@ -16,4 +16,4 @@ if [ "$INPUT_INCLUDE_PRODOS" == true ]; then
     cadius extractfile ProDOS_2_4_2.po /PRODOS.2.4.2/PRODOS  .
     cadius extractfile ProDOS_2_4_2.po /PRODOS.2.4.2/BASIC.SYSTEM  .
 fi
-
+ls
